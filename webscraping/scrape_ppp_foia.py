60d10eb7ba23d335e56717d4d34d36288dd75754
Jason Barhorst <jason.barhorst@knuggies.com>
2023-05-20 12:27:08 -0400

    Improved file management w/ ppp foia data scraping

--- webscraping/scrape_ppp_foia.py
+++ webscraping/scrape_ppp_foia.py
@@ -2,14 +2,30 @@
 scrape_ppp_foia.py
 script to scrape the SBA PPP FOIA site for the latest data
 data is available at https://data.sba.gov/dataset/ppp-foia
-This will be useful later to create an airflow DAG to automate the process
 """
 import os
+from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 
+date_format = "%Y-%m-%dT%H:%M:%S%z"
+
+# Create a directory to store the data if needed
+data_path = Path("./data/ppp_foia")
+data_path.mkdir(parents=True, exist_ok=True)
+data_date_file = data_path / 'DATA_DATE.txt'
+
+# Get data date for data already downloaded if it exists
+if os.path.exists(data_date_file):
+    with open(data_date_file, "r") as f:
+        stored_dd = datetime.strptime(f.readline(), date_format)
+else:
+    stored_dd = None
+print(f'stored data date: {stored_dd}')
+
+# Get the current date date from the website
 site = "https://data.sba.gov/dataset/ppp-foia"
 response = requests.get(site)
 
@@ -18,17 +34,14 @@ soup = BeautifulSoup(response.content, "html.parser")
 span_tag = soup.find("span", {"class": "automatic-local-datetime"})
 data_datetime = span_tag["data-datetime"]
 
-current_date = datetime.strptime(data_datetime, "%Y-%m-%dT%H:%M:%S%z")
-print("current_date:", current_date)
+current_dd = datetime.strptime(data_datetime, date_format)
+print("current data date:", current_dd)
 
-# TODO: Check if this date is newer than the previous download
-old_date = datetime.strptime("2023-04-05T14:27:21+0000", "%Y-%m-%dT%H:%M:%S%z")
-print("old_date:", old_date)
-
-if current_date > old_date:
-    print("Newer data available")
+new_data_available = stored_dd is None or current_dd > stored_dd
+if new_data_available:
+    print("Newer data available, downloading...")
 else:
-    print("You have the current data")
+    print("Current data already downloaded.")
     exit()
 
 # Iterate over all listed data sources
@@ -48,7 +61,7 @@ print("Number of data sources:", len(data_sources))
 
 # download the file
 for url in data_sources:
-    filename = f'./data/PPP-FOIA/{url.split("/")[-1]}'
+    filename = f'{data_path}/{url.split("/")[-1]}'
     if os.path.exists(filename):
         print(f'{url.split("/")[-1]} File already exists')
         continue
@@ -60,3 +73,7 @@ for url in data_sources:
                 print(f'{url.split("/")[-1]} File downloaded successfully')
         else:
             print("File download failed")
+
+# Write the current date to a file
+with open(data_date_file, "w") as f:
+    f.write(current_dd.strftime(date_format))
